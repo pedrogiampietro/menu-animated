5df159e4c051e96576859b37ccd01bd0bb33ffa9
Pedro Giampietro <pedrogiampietro@hotmail.com>
2024-09-25 18:08:15 -0300

    chore: clean project

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return <View style={styles.container}></View>;
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
